125ee81254c2c72c1669ee079ad6ee98e7007e42
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-11-28 19:23:46 +0600

    comments all await client and console log

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     const userCollection = client.db("taskManagementDB").collection("users");
     const taskCollection = client.db("taskManagementDB").collection("tasks");
@@ -176,5 +176,5 @@ app.get("/", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  // console.log(`Example app listening on port ${port}`);
 });
