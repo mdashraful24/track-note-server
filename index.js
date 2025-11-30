a47c66d938bcfaaa5349730a0035c176e1ee4dc2
mdashraful24 <mdashrafulislam2882@gmail.com>
2025-11-30 10:19:00 +0600

    updated code

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-require("dotenv").config();
-const cors = require("cors");
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 const app = express();
-app.use(cors());
-app.use(express.json());
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p8flg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
